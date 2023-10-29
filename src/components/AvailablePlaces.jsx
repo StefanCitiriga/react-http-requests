1986e358b6ca938110265f28b2b7ab2753317bd8
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2023-10-29 10:48:21 +0200

    Available places images are fetched

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,27 @@
 import Places from './Places.jsx';
+import {useState, useEffect} from 'react'
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    //this would be a get request
+  //fetch returns a promise
+  fetch('http://localhost:3000/places').then((response)=>{
+    return response.json();
+  }).then((resData) => {
+    setAvailablePlaces(resData.places);
+  })
+  },[]);
+
+  
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
