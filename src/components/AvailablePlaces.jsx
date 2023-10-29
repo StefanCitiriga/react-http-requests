90f5728776a691330d1a4242391ca93a8ac9fb93
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2023-10-29 13:12:38 +0200

    Displaying loading state during fetching

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,15 +4,29 @@ import {useState, useEffect} from 'react'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
+  // useEffect(() => {
+  //   //this would be a get request
+  // //fetch returns a promise
+  // fetch('http://localhost:3000/places').then((response)=>{
+  //   return response.json();
+  // }).then((resData) => {
+  //   setAvailablePlaces(resData.places);
+  // })
+  // },[]);
+
+  // async, await alternative
   useEffect(() => {
-    //this would be a get request
-  //fetch returns a promise
-  fetch('http://localhost:3000/places').then((response)=>{
-    return response.json();
-  }).then((resData) => {
-    setAvailablePlaces(resData.places);
-  })
+    setIsFetching(true);
+    async function fetchPlaces(){
+      const response= await fetch('http://localhost:3000/places');
+      const responseData= await response.json();
+      setAvailablePlaces(responseData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   },[]);
 
   
@@ -22,6 +36,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
